86add6c734af61325485e953a2b0e6427216fdf0
Bryce <brycesharp@brex.com>
2023-03-27 16:58:01 -0500

    Bryces Changes

--- src/domains/Settings/SettingsPage.tsx
+++ src/domains/Settings/SettingsPage.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@mui/material";
 import { MainContainer } from "components/MainPage.elements";
+
 import * as React from "react";
 
 export const SettingsPage = () => {
-  return <MainContainer>Welcome to the settings page!</MainContainer>;
+  return (
+    <MainContainer>
+      Welcome to the settings page!
+      <Button>Hello</Button>
+    </MainContainer>
+  );
 };
